0a7e535cdf19600d22f31af27b229c7571255b6e
Infernus <Infernus101@users.noreply.github.com>
2021-10-26 16:49:30 -0400

    more trees and problems

--- src/sample/sleepready/Problems.java
+++ src/sample/sleepready/Problems.java
@@ -1,5 +1,8 @@
 package sample.sleepready;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Problems {
 
     public static Integer findMaxInTree(Trees.BinaryTreeNode root, Integer max){
@@ -9,4 +12,62 @@ public class Problems {
 
         return Math.max(findMaxInTree(root.left, max), findMaxInTree(root.right, max));
     }
+
+    public static int treeSizeRecursive(Trees.BinaryTreeNode root){
+        if(root == null) return 0;
+        return 1 + treeSizeRecursive(root.left) + treeSizeRecursive(root.right);
+    }
+
+    public static void insertElementRecursive(Trees.BinaryTreeNode root, Integer toAdd){
+        if(root.data >= toAdd) {
+            if (root.left == null) {
+                root.left = new Trees.BinaryTreeNode(toAdd);
+            } else {
+                insertElementRecursive(root.right, toAdd);
+            }
+        }else {
+            if (root.right == null) {
+                root.right = new Trees.BinaryTreeNode(toAdd);
+            } else {
+                insertElementRecursive(root.left, toAdd);
+            }
+        }
+    }
+
+    public static boolean findElementRecursive(Trees.BinaryTreeNode root, Integer toFind){
+        if(root == null)
+            return false;
+
+        if(root.data == toFind)
+            return true;
+
+        return findElementRecursive(root.left, toFind) || findElementRecursive(root.right, toFind);
+    }
+
+    public static Integer findMaxInTreeIterative(Trees.BinaryTreeNode root){
+        // Using level order | BFS
+        if(root == null)
+            return Integer.MIN_VALUE;
+
+        Queue<Trees.BinaryTreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        int max = Integer.MIN_VALUE;
+
+        while(!q.isEmpty()){
+            Trees.BinaryTreeNode cur = q.poll();
+
+            max = Math.max(max, cur.data);
+
+            if(cur.left != null){
+                q.offer(cur.left);
+            }
+            if(cur.right != null){
+                q.offer(cur.right);
+            }
+        }
+
+        return max;
+    }
+
 }
--- src/sample/sleepready/Trees.java
+++ src/sample/sleepready/Trees.java
@@ -15,6 +15,33 @@ public class Trees {
         public BinaryTreeNode(int data) {
             this.data = data;
         }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            Queue<BinaryTreeNode> q = new LinkedList<>();
+            q.add(this);
+            q.add(null);
+
+            sb.append("Level order tree print - \n");
+            while(!q.isEmpty()){
+                BinaryTreeNode last = q.poll();
+                if(last != null) {
+                    sb.append(last.data).append(" ");
+                    if (last.left != null)
+                        q.add(last.left);
+                    if (last.right != null)
+                        q.add(last.right);
+                }else{
+                    if(!q.isEmpty()) {
+                        sb.append("\n");
+                        q.offer(null);
+                    }
+                }
+            }
+            return sb.toString();
+        }
     }
 
     public static ArrayList<Integer> inOrderRecursive(BinaryTreeNode root, ArrayList<Integer> output){
@@ -50,21 +77,58 @@ public class Trees {
     public static ArrayList<Integer> inorderIterative(BinaryTreeNode root){
         ArrayList<Integer> output = new ArrayList<>();
         Stack<BinaryTreeNode> s = new Stack<>();
-        BinaryTreeNode cur = root;
-        boolean done = false;
-        while (!done){
-            if(cur != null){ // Go to extreme left while pushing root
-                s.push(cur);
-                cur = cur.left;
-            }else{
-                if(s.isEmpty()){
-                    done = true;
-                }else {
-                    cur = s.pop();
-                    output.add(cur.data);
-                    cur = cur.right;
-                }
+
+        BinaryTreeNode curr = root;
+        while (!s.isEmpty() || curr != null) { // run till stack and curr both aren't empty or null
+            if (curr != null) { // go to extreme left
+                s.push(curr);
+                curr = curr.left;
             }
+            else {
+                curr = s.pop();
+                output.add(curr.data);
+
+                curr = curr.right;
+            }
+        }
+
+        return output;
+    }
+
+    public static ArrayList<Integer> postorderIterative(BinaryTreeNode root){
+        ArrayList<Integer> output = new ArrayList<>();
+        Stack<BinaryTreeNode> s1 = new Stack<>();
+        Stack<BinaryTreeNode> s2 = new Stack<>();
+
+        s1.push(root);
+        while(!s1.isEmpty()){
+            BinaryTreeNode cur = s1.pop();
+            s2.push(cur);
+
+            if(cur.left != null)
+                s1.push(cur.left);
+            if(cur.right != null)
+                s1.push(cur.right);
+        }
+
+        while(!s2.isEmpty()){ // has the elements in order
+            output.add(s2.pop().data);
+        }
+
+        return output;
+    }
+
+    public static ArrayList<Integer> preorderIterative(BinaryTreeNode root){
+        ArrayList<Integer> output = new ArrayList<>();
+        Stack<BinaryTreeNode> s = new Stack<>();
+        s.push(root);
+        while(!s.isEmpty()){
+            BinaryTreeNode cur = s.pop();
+            output.add(cur.data);
+            if(cur.right != null)
+                s.push(cur.right);
+            if(cur.left != null)
+                s.push(cur.left);
         }
 
         return output;
@@ -81,7 +145,7 @@ public class Trees {
         return output;
     }
 
-    public static ArrayList<Integer> levelOrder(BinaryTreeNode root){ // BFS
+    public static ArrayList<Integer> levelOrderIterative(BinaryTreeNode root){ // BFS
         ArrayList<Integer> output = new ArrayList<>();
         Queue<BinaryTreeNode> q = new LinkedList<>();
         q.add(root);
@@ -102,17 +166,34 @@ public class Trees {
     }
 
     public static void main(String[] args) {
-        //Sleepready
+        //Sleep ready codes
         BinaryTreeNode root = new BinaryTreeNode(1, new BinaryTreeNode(2, new BinaryTreeNode(4), new BinaryTreeNode(5)), new BinaryTreeNode(3, new BinaryTreeNode(6), new BinaryTreeNode(7)));
-        System.out.println("Level order / BFS " + levelOrder(root));
+        System.out.println("***** TREES *****");
+        System.out.println(root);
+        System.out.println("Level order Iterative / BFS " + levelOrderIterative(root)); // easiest
+
+        System.out.println("PreOrder Iterative - " + preorderIterative(root)); // easier than others, learn this to use
         System.out.println("InOrder Iterative - " + inorderIterative(root));
-        System.out.println("DFS - " + dfs(root, new ArrayList<>()));
+        System.out.println("PostOrder Iterative - " + postorderIterative(root)); // using 2 stacks, 1 stack is very hard
+
         System.out.println("PreOrder Recursive - " + preOrderRecursive(root, new ArrayList<>()));
         System.out.println("PostOrder Recursive - " + postOrderRecursive(root, new ArrayList<>()));
         System.out.println("InOrder Recursive - " + inOrderRecursive(root, new ArrayList<>()));
 
+        System.out.println("***** GRAPHS *****");
+        System.out.println("DFS - " + dfs(root, new ArrayList<>()));
+
         //Problems
-        System.out.println("Find Max - " + Problems.findMaxInTree(root, Integer.MIN_VALUE));
+        System.out.println("***** PROBLEMS *****");
+        System.out.println("Find Max Recursive - " + Problems.findMaxInTree(root, Integer.MIN_VALUE));
+        System.out.println("Find Max Iterative - " + Problems.findMaxInTreeIterative(root));
+        System.out.println("Find Element Recursive - " + Problems.findElementRecursive(root, 7));
+
+        Problems.insertElementRecursive(root, 8); // assuming root != null
+        System.out.println("Insert Element Recursive - " + root);
+
+        System.out.println("Size Recursive - " + Problems.treeSizeRecursive(root));
+
     }
 
 }
